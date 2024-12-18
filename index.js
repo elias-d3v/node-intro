0df797b944cee03e73bacd3cfce03b4b30e4a130
Elias-DEV <156446201+elias-d3v@users.noreply.github.com>
2024-12-18 13:47:52 +0100

    Interaction avec la base de donnée

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import express from 'express'
 import 'dotenv/config'
+import movieRouter from './routes/movieRouter.js'
+import mongoose from 'mongoose'
 
-const app = express();
-const port = process.env.PORT;
+const app = express()
+
+const PORT = process.env.PORT || 3002
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.use(movieRouter)
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World! Bienvenue sur mon serveur Express 🚀');
-});
+const MONGO_URI = 'mongodb+srv://chojiu:GA7bixQW5gH20yji@intro-express-mongo.d0wlzee.mongodb.net/?retryWrites=true&w=majority&appName=Intro-express-mongo'
 
-app.listen(port, () => {
-    console.log(`Serveur démarré sur http://localhost:${port}`);
-});
+mongoose.connect(MONGO_URI)
+const db = mongoose.connection
+db.on('success', () => {
+    console.log('Connected to the database 🟢')
+})
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.get('/', (request, response) => {
+    response.send(`Welcome to my API`)
+})
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
---
+++ routes/movieRouter.js
@@ -0,0 +1,61 @@
+import {Router} from 'express'
+import {movies} from '../data/movieData.js'
+
+
+const movieRouter = Router()
+
+movieRouter.get('/movies', (req, res) => {
+    return res.status(200).json(movies)
+})
+
+movieRouter.get('/movie/:id', (req, res) => {
+    let {id} = req.params
+    try{
+        const movieByID = movies.find(movie => movie.id === Number(id))
+        return res.json(movieByID)
+    }
+    catch(err){
+        return res.status(500).json({message : 'Internal server error'})
+    }
+})
+
+movieRouter.post('/movie', (req, res) => {
+    let {title, genre} = req.body
+    try{
+        if(!title, !genre){
+            return res.status(400).json({message : 'All fields are required'})
+        }
+        const newMovie = {
+            id : movies.length + 1,
+            title,
+            genre
+        }
+        movies.push(newMovie)
+        return res.status(201).json(movies)
+    }
+    catch(err){
+        return res.status(500).json({message : 'Internal server error'})
+    }
+})
+
+movieRouter.put('/movie/:id', (req, res) => {
+    let {id} = req.params
+    let {title, genre} = req.body
+    let movieByID = movies.find(movie => movie.id === parseInt(id))
+    console.log(movieByID)
+    movieByID.title = title || movieByID.title
+    movieByID.genre = genre
+    return res.status(201).json(movieByID)
+})
+
+
+movieRouter.delete('/movie/:id', (req, res) => {
+    let {id} = req.params
+    const movieByID = movies.find(movie => movie.id === parseInt(id))
+    const index = movies.indexOf(movieByID)
+    movies.splice(index, 1)
+    return res.status(203).json({message : 'Movie has been deleted'})
+})
+
+
+export default movieRouter
